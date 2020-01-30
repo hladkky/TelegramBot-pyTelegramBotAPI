d70b671458fbef6947490013c511144f542ad1b9
Hladkky <hladkky@gmail.com>
2020-01-30 13:47:03 +0200

    initial

--- bot.py
+++ bot.py
@@ -2,13 +2,90 @@ import telebot
 from telebot.types import Message
 from datetime import datetime
 
+
 TOKEN = '731991040:AAHKCcrcVRKDYvrPyupl8COe0JLLYtaKHk8'
 bot = telebot.TeleBot(TOKEN)
 
-idChat = '531381261'
-
+permissionsID = 393253446, 531381261
+bmd = 'Обробка та аналіз БМД'
+web = 'Веб-технології та веб-дизайн'
+cm = 'Чисельні методи'
+oop = 'ООП'
+bi = 'Біоінформатика-1. Основи МББ'
+en = 'Іноземна мова'
+homework = {bmd: None,
+            web: None,
+            cm: None,
+            oop: None,
+            bi: None,
+            en: None}
+buffer = {}
 
 first_saturday = datetime(2020, 1, 18)
+
+
+@bot.message_handler(commands=['homework'])
+def send_homework(message):
+    answer = ''
+    for d, hw in homework.items():
+        if not hw:
+            hw = '-'
+        answer += f'*{d}*:\n   {hw}\n'
+    bot.send_message(message.chat.id, answer, parse_mode='Markdown')
+
+
+@bot.message_handler(commands=['sethomework'])
+def set_homework(message: Message):
+    if message.chat.type != 'private':
+        bot.send_message(message.chat.id, 'Команда доступна в приватних повідомленнях.')
+    elif message.from_user.id not in permissionsID:
+        bot.send_message(message.chat.id, 'Відсутні права доступу.')
+    else:
+        markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        markup.add(bmd, web, cm, oop, bi, en)
+        msg = bot.send_message(message.chat.id, 'Дисципліна:', reply_markup=markup)
+        bot.register_next_step_handler(msg, name_step)
+
+
+def name_step(message):
+    name = message.text
+    if name not in homework.keys():
+        bot.reply_to(message, 'Предмет відсутній', reply_markup=telebot.types.ReplyKeyboardRemove())
+        return
+    buffer['name'] = name
+    print(buffer)
+    reply = 'Додати, перезаписати, видалити?'
+    markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    markup.add('Додати', 'Перезаписати', 'Видалити')
+    msg = bot.reply_to(message, text=reply, reply_markup=markup)
+    bot.register_next_step_handler(msg, set_step)
+
+
+def set_step(message):
+    if message.text == 'Видалити':
+        homework[buffer['name']] = None
+        bot.send_message(message.chat.id,
+                         text='Успішно видалено!',
+                         reply_markup=telebot.types.ReplyKeyboardRemove())
+    else:
+        buffer['mode'] = message.text
+        msg = bot.send_message(message.chat.id, text='Нове завдання:')
+        bot.register_next_step_handler(msg, new_task_step)
+
+
+def new_task_step(message):
+    discipline = buffer['name']
+    if not homework[discipline]:
+        homework[discipline] = message.text
+    else:
+        rewrite = 1 if buffer['mode'] == 'Перезаписати' else 0
+        if rewrite:
+            homework[discipline] = message.text
+        else:
+            homework[discipline] += f'\n   {message.text}'
+        bot.send_message(message.chat.id,
+                         text='Успішно оновлено!',
+                         reply_markup=telebot.types.ReplyKeyboardRemove())
 
 
 @bot.message_handler(commands=['today', 'tomorrow'])
@@ -33,6 +110,9 @@ def send_today_schedule(message: Message):
 
 @bot.message_handler(commands=['time'])
 def send_schedule(message):
+
+    print(message.from_user)
+
     fmt = '%H:%M:%S'
     bot.reply_to(message, text=datetime.now().strftime(fmt))
 
@@ -56,6 +136,5 @@ def send_teachers(message):
         bot.send_message(message.chat.id,
                          text=answer,
                          parse_mode='Markdown')
-
 
 bot.polling()
